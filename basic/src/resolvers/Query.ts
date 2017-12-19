4d55137f6a0e9f92ffb97d1930ed1e353cd2cde3
Johannes Schickling <schickling.j@gmail.com>
2017-12-19 18:09:48 +0100

    imported typescript boilerplates

---
+++ basic/src/resolvers/Query.ts
@@ -0,0 +1,12 @@
+import { getUserId, Context } from '../utils'
+
+export const Query = {
+  feed(parent, args, ctx: Context, info) {
+    return ctx.db.query.posts({ where: { isPublished: true } }, info)
+  },
+
+  me(parent, args, ctx: Context, info) {
+    const id = getUserId(ctx)
+    return ctx.db.query.user({ where: { id } }, info)
+  },
+}
