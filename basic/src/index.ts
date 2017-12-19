23f27c016cdfd8f69696b5d4df1e2a1fc53ca2be
Johannes Schickling <schickling.j@gmail.com>
2017-12-19 22:58:40 +0100

    generated schema

--- basic/src/index.ts
+++ basic/src/index.ts
@@ -36,9 +36,9 @@ const server = new GraphQLServer({
   context: req => ({
     ...req,
     db: new Graphcool({
-      schemaPath: './database/schema.graphql',
+      schemaPath: './database/schema.generated.graphql',
       endpoint: 'http://localhost:60000/api/graphql-boilerplate/dev',
-      secret: 'your-graphcool-secret',
+      secret: 'secret123',
     }),
   }),
 })
