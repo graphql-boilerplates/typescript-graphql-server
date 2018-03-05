e57204fc19fe9fcef7b6f29716ac45b8ab9d2b20
Devan Beitel <devan@devanb.us>
2018-03-05 10:36:21 -0500

    Adds minimal boilerplate.

---
+++ minimal/src/index.ts
@@ -0,0 +1,23 @@
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => {
+      const returnValue = !name ? `Hello ${name || 'World!'}` : null;
+      return returnValue;
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => console.log('Server is running on http://localhost:4000'));
