33d91e2171350b7e981878ee7f3d7ad3087b27d1
Nikolas Burk <nikolas.burk@gmail.com>
2018-04-25 10:31:30 +0200

    fix issue in advanced boilerplate, update generated files in basic and update deps

--- advanced/src/index.ts
+++ advanced/src/index.ts
@@ -8,9 +8,9 @@ const server = new GraphQLServer({
   context: req => ({
     ...req,
     db: new Prisma({
-      endpoint: '__PRISMA_ENDPOINT__', // the endpoint of the Prisma API
+      endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API (value set in `.env`)
       debug: true, // log all GraphQL queries & mutations sent to the Prisma API
-      // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
+      // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
     }),
   }),
 })
