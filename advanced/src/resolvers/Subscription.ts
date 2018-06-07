06c582c2039312a1c3d9c73fbd0cfaebb99204d5
Nikolas Burk <nikolas.burk@gmail.com>
2018-06-07 16:10:35 +0200

    add proper subscriptions

--- advanced/src/resolvers/Subscription.ts
+++ advanced/src/resolvers/Subscription.ts
@@ -1,29 +1,18 @@
 import { Context } from '../utils'
 
 export const Subscription = {
-    feedSubscription: {
-      subscribe: (parent, args, ctx: Context, info) => {
-        return ctx.db.subscription.post({}, info)
-      },
+  feedSubscription: {
+    subscribe: (parent, args, ctx: Context, info) => {
+      return ctx.db.subscription.post(
+        {
+          where: {
+            node: {
+              isPublished: true,
+            },
+          },
+        },
+        info,
+      )
     },
-  }
-  
-  // This is the same subscription as above but only fires for 
-  // posts that have been published.
-  // export const Subscription = {
-  //   feedSubscription: {
-  //     subscribe: (parent, args, ctx: Context, info) => {
-  //       return ctx.db.subscription.post(
-  //         {
-  //           where: {
-  //             node: {
-  //               isPublished: true,
-  //             },
-  //           },
-  //         },
-  //         info,
-  //       )
-  //     },
-  //   },
-  // }
-  
+  },
+}
